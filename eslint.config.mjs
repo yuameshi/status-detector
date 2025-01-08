3dc4b67cf9e1a692049f6e396b685f04c9187ac5
糸 <64469437+yuameshi@users.noreply.github.com>
2025-01-08 17:08:12 +0800

    chore: remove template folder & configure eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,31 @@ const compat = new FlatCompat({
 	baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')];
+const eslintConfig = [
+	...compat.extends('next/core-web-vitals', 'next/typescript'),
+	{
+		root: true,
+		rules: {
+			// 允许被禁止类型
+			'@typescript-eslint/ban-types': 'off',
+			// 允许any
+			'@typescript-eslint/no-explicit-any': 'off',
+			// 允许@ts-xxx
+			'@typescript-eslint/ban-ts-comment': 'off',
+			// 允许空函数
+			'@typescript-eslint/no-empty-function': 'off',
+			// 允许下划线开头的不使用变量
+			'no-unused-vars': [
+				'warn',
+				{
+					argsIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+				},
+			],
+			// 关掉，不然一个错误会变俩
+			'@typescript-eslint/no-unused-vars': ['off'],
+		},
+	},
+];
 
 export default eslintConfig;

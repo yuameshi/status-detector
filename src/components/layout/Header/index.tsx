2425b4007b53d5fbc115cd107928ac4cb9f1cd9b
糸 <64469437+yuameshi@users.noreply.github.com>
2025-01-10 16:10:43 +0800

    fix(Header): incorrect ui at md- displays & shrink code

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,67 +1,50 @@
-import { AppBar, Box, Toolbar, Typography, Grid2, Button } from '@mui/material';
-import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import { AppBar, Toolbar, Grid2, Button } from '@mui/material';
 import { DarkmodeToggle } from '@components/layout/Header/DarkmodeToggle';
 import { LanguageMenu } from '@components/layout/Header/LanguageMenu';
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import { externalLinks } from '@constants/config';
 import { Title } from '@components/layout/Header/Title';
+import StorageIcon from '@mui/icons-material/Storage';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 export const AppHeader = () => {
 	return (
 		<AppBar position='sticky'>
 			<Toolbar>
-				<QueryStatsIcon sx={{ m: 2 }} />
+				<StorageIcon sx={{ m: 2 }} />
 				<Grid2
 					container
 					sx={{
 						display: 'flex',
-						justifyContent: 'center',
-						alignItems: 'center',
 						width: '100%',
+						justifyContent: 'space-between',
 					}}
 				>
 					<Grid2
-						size={{
-							xs: 12,
-							lg: 'auto',
-						}}
+						size='auto'
+						display='flex'
+						alignItems='center'
 					>
 						<Title />
-					</Grid2>
-					<Grid2>
-						<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-							{externalLinks.map(({ url, name }, i) => (
-								<Button
-									key={i}
-									href={url}
-									target='_blank'
-									rel='noopener noreferrer'
-									sx={{ my: 2, color: 'white' }}
-									endIcon={<OpenInNewIcon />}
-								>
-									{name}
-								</Button>
-							))}
-						</Box>
+						{externalLinks.map(({ url, name }, i) => (
+							<Button
+								key={i}
+								href={url}
+								target='_blank'
+								rel='noopener noreferrer'
+								sx={{ my: 2, color: 'white' }}
+								endIcon={<OpenInNewIcon />}
+							>
+								{name}
+							</Button>
+						))}
 					</Grid2>
 					<Grid2
-						size={{
-							xs: 0,
-							lg: 'grow',
-						}}
+						size='auto'
+						display='flex'
+						alignItems='center'
 					>
-						<Box sx={{ flexGrow: 1 }} />
-					</Grid2>
-					<Grid2
-						size={{
-							xs: 12,
-							lg: 'auto',
-						}}
-					>
-						<Box>
-							<LanguageMenu />
-							<DarkmodeToggle />
-						</Box>
+						<LanguageMenu />
+						<DarkmodeToggle />
 					</Grid2>
 				</Grid2>
 			</Toolbar>

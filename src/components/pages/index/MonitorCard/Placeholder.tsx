345765f5c2c9ddf5f850f0cbbf4be29b0558b609
糸 <64469437+yuameshi@users.noreply.github.com>
2025-01-10 16:44:41 +0800

    chore(MonitorCard): remove unused variables & imports

--- src/components/pages/index/MonitorCard/Placeholder.tsx
+++ src/components/pages/index/MonitorCard/Placeholder.tsx
@@ -1,87 +1,81 @@
-import { Box, Card, CardContent, Grid2, Paper, Skeleton, Tooltip, Typography } from '@mui/material';
-import { useTranslations } from 'next-intl';
-import CheckIcon from '@mui/icons-material/Check';
-import { DetailedStatusCell } from './DetailedStatusCell';
+import { Box, Card, CardContent, Grid2, Skeleton } from '@mui/material';
+import type { FC } from 'react';
 
-export function MonitorCardPlaceholder() {
-	const t = useTranslations('index');
-
-	return (
-		<Card
-			sx={{
-				width: '100%',
-				maxWidth: 1000,
-				my: 1.5,
-			}}
-		>
-			<CardContent>
-				<Box>
-					<Grid2
-						container
-						sx={{
-							mb: 1,
-						}}
-					>
-						<Grid2>
-							<Skeleton
-								variant='rounded'
-								width={150}
-							/>
-						</Grid2>
-						<Grid2 size='grow' />
-						<Grid2
-							sx={{
-								display: 'inline-flex',
-								alignItems: 'center',
-								color: 'success.main',
-							}}
-						>
-							<Skeleton
-								variant='circular'
-								width={18}
-								sx={{ mx: 0.5 }}
-							/>
-							<Skeleton
-								variant='rounded'
-								width={35}
-							/>
-						</Grid2>
+export const MonitorCardPlaceholder: FC = () => (
+	<Card
+		sx={{
+			width: '100%',
+			maxWidth: 1000,
+			my: 1.5,
+		}}
+	>
+		<CardContent>
+			<Box>
+				<Grid2
+					container
+					sx={{
+						mb: 1,
+					}}
+				>
+					<Grid2>
+						<Skeleton
+							variant='rounded'
+							width={150}
+						/>
 					</Grid2>
+					<Grid2 size='grow' />
 					<Grid2
-						container
-						columns={60}
-						columnSpacing={0.4}
-						height={30}
-					>
-						{new Array(60).fill('').map((_, i) => (
-							<Grid2
-								key={i}
-								size='grow'
-								sx={{
-									height: '100%',
-								}}
-							>
-								<Skeleton
-									variant='rounded'
-									sx={{ height: '100%', width: '100%' }}
-								/>
-							</Grid2>
-						))}
-					</Grid2>
-					<Box
 						sx={{
-							display: 'flex',
-							justifyContent: 'center',
-							mt: 1,
+							display: 'inline-flex',
+							alignItems: 'center',
+							color: 'success.main',
 						}}
 					>
 						<Skeleton
-							variant='text'
-							width={175}
+							variant='circular'
+							width={18}
+							sx={{ mx: 0.5 }}
 						/>
-					</Box>
+						<Skeleton
+							variant='rounded'
+							width={35}
+						/>
+					</Grid2>
+				</Grid2>
+				<Grid2
+					container
+					columns={60}
+					columnSpacing={0.4}
+					height={30}
+				>
+					{new Array(60).fill('').map((_, i) => (
+						<Grid2
+							key={i}
+							size='grow'
+							sx={{
+								height: '100%',
+							}}
+						>
+							<Skeleton
+								variant='rounded'
+								sx={{ height: '100%', width: '100%' }}
+							/>
+						</Grid2>
+					))}
+				</Grid2>
+				<Box
+					sx={{
+						display: 'flex',
+						justifyContent: 'center',
+						mt: 1,
+					}}
+				>
+					<Skeleton
+						variant='text'
+						width={175}
+					/>
 				</Box>
-			</CardContent>
-		</Card>
-	);
-}
+			</Box>
+		</CardContent>
+	</Card>
+);

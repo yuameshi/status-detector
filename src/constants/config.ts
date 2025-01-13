aab6a03b7c30ad84ec73af8628cbf061abe3a261
糸 <64469437+yuameshi@users.noreply.github.com>
2025-01-13 19:25:21 +0800

    fix(conf): will filter empty items

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,14 +1,20 @@
-export const apiKeys: string[] = process.env.API_KEYS?.split(',').map(key => key.trim()) || [];
+export const apiKeys: string[] =
+	process.env.API_KEYS?.split(',')
+		.map(key => key.trim())
+		.filter(key => key !== '') || [];
 
 export const showLinks: boolean = process.env.SHOW_LINKS === 'true';
 
 export const maxDays: number = Number(process.env.MAX_DAYS) || 60;
 
 export const externalLinks: ExternalLink[] =
-	process.env.EXTERNAL_LINKS?.split('_').map(el => ({
-		name: el.split('|')[0],
-		url: el.split('|')[1],
-	})) || [];
+	process.env.EXTERNAL_LINKS?.split('_')
+		.map(link => link.trim())
+		.filter(link => link !== '')
+		.map(link => ({
+			name: link.split('|')[0],
+			url: link.split('|')[1],
+		})) || [];
 
 export const config = {
 	apiKeys,
